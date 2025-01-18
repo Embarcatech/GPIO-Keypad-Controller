e5bead3cca1897eeeecfb0f8d5d8c65a931b0dd2
vitorlion14 <joaovitorsoares1407@gmail.com>
2025-01-18 15:02:55 -0300

    inserir o gpio_put nos leds na main  principal, pois o led azul  não tava acendendo.

--- GPIO-Keypad-Controller.c
+++ GPIO-Keypad-Controller.c
@@ -7,8 +7,8 @@
 #define LED_R_PIN 13 //PINO DO LED VERMELHO
 
 // Matrizes com nomes de colunas e linhas
-const uint LINHAS[4] = {1, 2, 3, 4};
-const uint COLUNAS[4] = {5, 6, 7, 8};
+const uint LINHAS[4] = {8, 7, 6, 5}; 
+const uint COLUNAS[4] = {4, 3, 2, 1};
 
 
 // Mapeamento das teclas em uma matriz 4x4
@@ -29,34 +29,34 @@ void iniciar_teclado() {
         gpio_put(LINHAS[i], 0);
     }
 
-    // Definição das colunas como entradas com resistores de pull-down
+    // Definição das colunas como entradas com resistores de pull-up
     for (int i = 0; i < 4; i++) {
         gpio_init(COLUNAS[i]);
         gpio_set_dir(COLUNAS[i], GPIO_IN);
-        gpio_pull_down(COLUNAS[i]);
+        gpio_pull_up(COLUNAS[i]);
     }
 }
 
 // Varredura do teclado e retorno da tecla pressionada
 char leitura_teclado() {
     for (int row = 0; row < 4; row++) {
-        // Coloca a linha atual em nível alto
-        gpio_put(LINHAS[row], 1);
+        // Coloca a linha atual em nível baixo
+        gpio_put(LINHAS[row], 0);
 
         for (int col = 0; col < 4; col++) {
             // Verifica se a tecla foi pressionada
-            if (gpio_get(COLUNAS[col])) {
+            if (!gpio_get(COLUNAS[col])) {
                 // Espera um tempo para estabilização da tecla pressionada
-                sleep_ms(50);
+                sleep_ms(150);
 
-                gpio_put(LINHAS[row], 0); // Reseta a linha atual
+                gpio_put(LINHAS[row], 1); // Reseta a linha atual
 
                 return teclas[row][col];
             }
         }
 
-        // Coloca a linha atual novamente para nível baixo
-        gpio_put(LINHAS[row], 0);
+        // Coloca a linha atual novamente para nível alto
+        gpio_put(LINHAS[row], 1);
     }
 
     return 0; // Nenhuma tecla pressionada
@@ -72,35 +72,36 @@ int main() {
     
     // Inicializa o teclado
     iniciar_teclado();
-// Inicializa os LEDs
+    // Inicializa os LEDs
     gpio_init(LED_R_PIN); //inicia o vermelho
     gpio_set_dir(LED_R_PIN, GPIO_OUT);
-    gpio_init(XXXXXXXXX);  // inicia o verde
-    gpio_set_dir(XXXXXXX,XXXXXX);
-    gpio_init(XXXXXXX);  // inicia o azul
-    gpio_set_dir(XXXXXXX, XXXXXXX);
+    gpio_put(LED_R_PIN, 0);
+    gpio_init(LED_G_PIN); //inicia o verde
+    gpio_set_dir(LED_G_PIN, GPIO_OUT);
+    gpio_put(LED_G_PIN, 0);
+    gpio_init(LED_B_PIN); //Inicia o Led azul
+    gpio_set_dir(LED_B_PIN, GPIO_OUT);
+    gpio_put(LED_B_PIN, 0);
 
     while (true) {
         char tecla = leitura_teclado();
         printf("Tecla pressionada: %c\n", tecla);
-        
-
-
-        
+               
         // Configuração dos LEDs com base na tecla pressionada
         switch (tecla) {
             case 'A':
                 set_leds(1, 0, 0); // Botão A acende o LED vermelho
                 break;
-            case 'B':
-                set_leds(x, x, x); // Botão B acende o LED azul
+           case 'B':
+                set_leds(0, 0, 1); // Botão B acende o Led azul
                 break;
             case 'C':
-                set_leds(x, x, x); // Botão C acende o LED verde
+                set_leds(0, 1, 0); //Botão C acende o LED verde
                 break;
             case 'D':
-                set_leds(1, 1, 1); // Botão D acende todos os LEDs
+                set_leds (1,1,1); // Botão D acende todos os LEDS
                 break;
+                
             default:
                 set_leds(0, 0, 0); // Desliga todos os LEDs
                 break;
@@ -112,5 +113,3 @@ int main() {
 
     return 0;
 }
-
-
